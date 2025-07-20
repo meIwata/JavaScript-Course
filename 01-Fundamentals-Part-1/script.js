ee592e8e25a42f345f957d1993e5e20dbf9d0d1d
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-07-20 23:07:25 +0800

    The switch Statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -294,6 +294,7 @@ Your tasks:
 TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
 */
 
+/*
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoalas = (88 + 91 + 110) / 3;
 console.log(scoreDolphins, scoreKoalas);
@@ -303,4 +304,48 @@ if (scoreDolphins > scoreKoalas) {
     console.log("Koalas win the trophy");
 } else if (scoreDolphins === scoreKoalas) {
     console.log("Both win the trophy");
+}
+
+*/
+
+// switch 語句
+const day = 'wednesday';
+switch (day) {
+    case 'monday': // 如果 day 是 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default: // 如果 day 不是上述任何一個值
+        console.log('Not a valid day!');
+}
+
+
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
 }

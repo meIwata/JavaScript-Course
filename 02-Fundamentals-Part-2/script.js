335a41922d937623b8a86bc00c40089997f38587
meIwata <waleilulu@gmail.com>
2025-07-21 11:30:30 +0800

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,9 @@
+'use strict'; // 使用嚴格模式
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;

8fbd7119b0c7dfde42e9a29f6af01e6c1a4f30fe
meIwata <waleilulu@gmail.com>
2025-07-21 11:56:34 +0800

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,4 +1,5 @@
 'use strict'; // 使用嚴格模式
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -7,3 +8,28 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+function logger(){
+    console.log('My name is Luis');
+}
+// 呼叫方法
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) { // 定義了一個名為 fruitProcessor 的函數，有兩個參數
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice; // 將這個果汁描述字串作為結果傳回
+}
+
+const appleJuice = fruitProcessor(5, 0); // 呼叫這個函數，傳入5顆蘋果和0顆橘子
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0)); // 直接呼叫函數並印出結果
+
+const appleOrangeJuice = fruitProcessor(2, 4); // 呼叫這個函數，傳入2顆蘋果和4顆橘子
+console.log(appleOrangeJuice); // 印出結果
+
+const num = Number('23'); // 將字串 '23' 轉換為數字
+console.log(num); // 印出數字 23

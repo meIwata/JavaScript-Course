1c1ec7c919bd6058488640c3ecdc044943d5106a
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-07-27 21:42:11 +0800

    Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -221,7 +221,7 @@ for (let i = 0; i < bills.length; i++) {
     const total = bills[i] + tips[i];
     console.log(`帳單：${bills[i]}，小費：${tips[i]}，總金額：${total}`);
 }
-*/
+
 
 // Objects 鍵值對
 const jonas = {
@@ -231,3 +231,39 @@ const jonas = {
     job: 'teacher',
     friends: ['Michael', 'Steven', 'Peter']
 };
+*/
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Steven', 'Peter']
+};
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']); // 使用方括號語法訪問屬性
+console.log(jonas);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]); // 使用變數來動態訪問屬性
+
+// 也可以這樣寫，因為陣列索引從0開始
+console.log(jonas.friends[jonas.friends.length - 1]); // console.log(jonas.friends[2]); 
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends.');
+
+if (jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends.');
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael."
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`);

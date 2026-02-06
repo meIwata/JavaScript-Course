2429f9db09209c7266db935e74ed2e2c515ddfd5
waleilulu <waleilulu@gmail.com>
2026-02-06 13:07:28 +0800

    PROJECT #1: Guess My Number!

---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// console.log(document.querySelector('.message')); // querySelector()是選取器，類似CSS選擇器，會回傳第一個符合條件的元素
+console.log(document.querySelector('.message').textContent); // textContent是元素的文本內容
+

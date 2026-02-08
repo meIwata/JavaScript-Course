9a41c94da29fefba1abe96ce8583f57cb9d4d60c
waleilulu <waleilulu@gmail.com>
2026-02-08 22:45:58 +0800

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -3,3 +3,10 @@
 // console.log(document.querySelector('.message')); // querySelector()是選取器，類似CSS選擇器，會回傳第一個符合條件的元素
 console.log(document.querySelector('.message').textContent); // textContent是元素的文本內容
 
+document.querySelector('.message').textContent = '嘿嘿嘿!!!'; // 可以直接修改元素的文本內容
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess')
+document.querySelector('.guess').value = 23; // 可以直接修改輸入框的值
+console.log(document.querySelector('.guess').value); // value是輸入框的值

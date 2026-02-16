123fe07b8a08d488251894845bd81a60d41850dd
waleilulu <waleilulu@gmail.com>
2026-02-17 00:02:18 +0800

    Holding Current Score

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -24,6 +24,8 @@ diceEl.classList.add('hidden'); // 隱藏骰子圖片
 const scores = [0, 0]; // 玩家 1 和玩家 2 的總分數
 let currentScore = 0; // 當前玩家的暫時分數
 let activePlayer = 0; // 0 代表玩家 1，1 代表玩家 2
+let playing = true; // 遊戲狀態，true 代表遊戲進行中，false 代表遊戲結束
+
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0; // 更新當前玩家的暫時分數顯示為 0
     activePlayer = activePlayer === 0 ? 1 : 0; // 切換玩家
@@ -37,46 +39,56 @@ const switchPlayer = function () {
 
 /* 產生隨機骰子點數 */
 btnRoll.addEventListener('click', () => {
-    // 1. 產生隨機骰子點數
-    const dice = Math.trunc(Math.random() * 6) + 1; // 產生 1~6 的隨機整數
-    // console.log(`骰子點數: ${dice}`); // 在控制台顯示骰子點數
+    if (playing) {
+        // 1. 產生隨機骰子點數
+        const dice = Math.trunc(Math.random() * 6) + 1; // 產生 1~6 的隨機整數
+        // console.log(`骰子點數: ${dice}`); // 在控制台顯示骰子點數
 
-    // 2. 顯示骰子圖片
-    diceEl.classList.remove('hidden'); // 顯示骰子圖片
-    diceEl.src = `dice-${dice}.png`; // 使用src屬性來改變圖片來源，顯示對應點數的骰子圖片
+        // 2. 顯示骰子圖片
+        diceEl.classList.remove('hidden'); // 顯示骰子圖片
+        diceEl.src = `dice-${dice}.png`; // 使用src屬性來改變圖片來源，顯示對應點數的骰子圖片
 
-    // 3. 判斷如果骰子點數是 1，則切換玩家
-    if (dice !== 1) {
-        // 如果骰子點數不是 1，則將點數加到當前玩家的暫時分數上
-        currentScore += dice; // 將骰子點數加到當前玩家的暫時分數上
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore; // 更新當前玩家的暫時分數顯示
+        // 3. 判斷如果骰子點數是 1，則切換玩家
+        if (dice !== 1) {
+            // 如果骰子點數不是 1，則將點數加到當前玩家的暫時分數上
+            currentScore += dice; // 將骰子點數加到當前玩家的暫時分數上
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore; // 更新當前玩家的暫時分數顯示
 
-    } else {
-        switchPlayer(); // 切換玩家
-        /*
-        // 如果骰子點數是 1，則切換玩家
-        document.getElementById(`current--${activePlayer}`).textContent = 0; // 更新當前玩家的暫時分數顯示為 0
-        activePlayer = activePlayer === 0 ? 1 : 0; // 切換玩家
-        currentScore = 0; // 重置暫時分數
+        } else {
+            switchPlayer(); // 切換玩家
+            /*
+            // 如果骰子點數是 1，則切換玩家
+            document.getElementById(`current--${activePlayer}`).textContent = 0; // 更新當前玩家的暫時分數顯示為 0
+            activePlayer = activePlayer === 0 ? 1 : 0; // 切換玩家
+            currentScore = 0; // 重置暫時分數
 
-        // toggle 方法會自動檢查元素是否具有指定的 class，如果有就移除，沒有就添加，因此可以用來切換玩家的 active 狀態
-        player0El.classList.toggle('player--active'); // 切換玩家 1 的 active 狀態
-        player1El.classList.toggle('player--active'); // 切換玩家 2 的 active 狀態
-         */
+            // toggle 方法會自動檢查元素是否具有指定的 class，如果有就移除，沒有就添加，因此可以用來切換玩家的 active 狀態
+            player0El.classList.toggle('player--active'); // 切換玩家 1 的 active 狀態
+            player1El.classList.toggle('player--active'); // 切換玩家 2 的 active 狀態
+             */
+        }
     }
 });
 
 btnHold.addEventListener('click', function () {
-    // console.log(`玩家 ${activePlayer} 按下 Hold 按鈕`); // 在控制台顯示當前玩家按下 Hold 按鈕
-    // 1. 將當前玩家的暫時分數加到總分數上
-    scores[activePlayer] += currentScore; // 將當前玩家的暫時分數加到總分數上
-    // scores[1] = scores[1] + currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]; // 更新當前玩家的總分數顯示
+    if (playing) {
+        // console.log(`玩家 ${activePlayer} 按下 Hold 按鈕`); // 在控制台顯示當前玩家按下 Hold 按鈕
+        // 1. 將當前玩家的暫時分數加到總分數上
+        scores[activePlayer] += currentScore; // 將當前玩家的暫時分數加到總分數上
+        // scores[1] = scores[1] + currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]; // 更新當前玩家的總分數顯示
 
 
-    // 2. 檢查玩家的總分數是否 >= 100
-    // 如果是，則該玩家獲勝，遊戲結束
-
-    // 如果不是，則切換玩家
-    switchPlayer(); // 切換玩家
-})
+        // 2. 檢查玩家的總分數是否 >= 100
+        if (scores[activePlayer] >= 20) {
+            // 如果是，則該玩家獲勝，遊戲結束
+            playing = false;
+            diceEl.classList.add('hidden'); // 隱藏骰子圖片
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner'); // 為獲勝玩家的區塊添加 winner class
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active'); // 移除獲勝玩家的 active class
+        } else {
+            // 如果不是，則切換玩家
+            switchPlayer(); // 切換玩家
+        }
+    }
+});

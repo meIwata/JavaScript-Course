b6ddc599696e1add2a25c2fb80a1ccf5386a3360
waleilulu <waleilulu@gmail.com>
2026-02-16 23:48:55 +0800

    Holding Current Score

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -24,6 +24,16 @@ diceEl.classList.add('hidden'); // 隱藏骰子圖片
 const scores = [0, 0]; // 玩家 1 和玩家 2 的總分數
 let currentScore = 0; // 當前玩家的暫時分數
 let activePlayer = 0; // 0 代表玩家 1，1 代表玩家 2
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0; // 更新當前玩家的暫時分數顯示為 0
+    activePlayer = activePlayer === 0 ? 1 : 0; // 切換玩家
+    currentScore = 0; // 重置暫時分數
+
+    // toggle 方法會自動檢查元素是否具有指定的 class，如果有就移除，沒有就添加，因此可以用來切換玩家的 active 狀態
+    player0El.classList.toggle('player--active'); // 切換玩家 1 的 active 狀態
+    player1El.classList.toggle('player--active'); // 切換玩家 2 的 active 狀態
+};
+
 
 /* 產生隨機骰子點數 */
 btnRoll.addEventListener('click', () => {
@@ -42,13 +52,31 @@ btnRoll.addEventListener('click', () => {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore; // 更新當前玩家的暫時分數顯示
 
     } else {
+        switchPlayer(); // 切換玩家
+        /*
         // 如果骰子點數是 1，則切換玩家
         document.getElementById(`current--${activePlayer}`).textContent = 0; // 更新當前玩家的暫時分數顯示為 0
-        activePlayer = activePlayer === 0? 1 : 0; // 切換玩家
+        activePlayer = activePlayer === 0 ? 1 : 0; // 切換玩家
         currentScore = 0; // 重置暫時分數
 
         // toggle 方法會自動檢查元素是否具有指定的 class，如果有就移除，沒有就添加，因此可以用來切換玩家的 active 狀態
         player0El.classList.toggle('player--active'); // 切換玩家 1 的 active 狀態
         player1El.classList.toggle('player--active'); // 切換玩家 2 的 active 狀態
+         */
     }
+});
+
+btnHold.addEventListener('click', function () {
+    // console.log(`玩家 ${activePlayer} 按下 Hold 按鈕`); // 在控制台顯示當前玩家按下 Hold 按鈕
+    // 1. 將當前玩家的暫時分數加到總分數上
+    scores[activePlayer] += currentScore; // 將當前玩家的暫時分數加到總分數上
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]; // 更新當前玩家的總分數顯示
+
+
+    // 2. 檢查玩家的總分數是否 >= 100
+    // 如果是，則該玩家獲勝，遊戲結束
+
+    // 如果不是，則切換玩家
+    switchPlayer(); // 切換玩家
 })

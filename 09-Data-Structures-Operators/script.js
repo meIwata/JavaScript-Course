8901068113f3f5aa5f211fdb1269ec0f28c91623
waleilulu <waleilulu@gmail.com>
2026-04-20 22:39:15 +0800

    CHALLENGE #3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -682,6 +682,7 @@ console.log(rest.size);
 console.log(rest.get(arr)); // 這裡的arr是一個陣列，當我們將它作為Map的鍵時，Map會將它轉換為一個物件，這樣就無法通過原來的arr來訪問對應的值了，所以這裡會返回undefined
 */
 
+/*
 const question = new Map([
     ['question', 'What is the best programming language in the world?'],
     [1, 'C'],
@@ -713,3 +714,56 @@ console.log([...question]);
 console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
+*/
+
+// Coding Challenge #3
+
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
+]);
+
+// 1.
+// console.log(gameEvents.values());
+const events = [...new Set(gameEvents.values())]; // 展開運算符將gameEvents.values()中的元素展開成獨立的元素，然後再傳入Set中，這樣就會自動去除重複的元素，最後再使用展開運算符將Set中的元素展開成獨立的元素，然後再放入一個新的陣列中，這樣就得到了events陣列，其中包含了gameEvents.values()中的唯一元素
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+    `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+    const half = min <= 45 ? 'FIRST' : 'SECOND';
+    console.log(`[${half} HALF] ${min}: ${event}`);
+}
